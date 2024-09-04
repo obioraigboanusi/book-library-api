d02a8d4e7121b782efe1fb87532a38a221809f72
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-09-04 11:34:07 +0100

    Set up linting and formatting

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
-
-
 const app = express();
 
-app.use(cors({origin:true, credentials:true}));
-app.use(express.json({extended: false}));
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
