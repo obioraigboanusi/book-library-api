8984a5a93923e520d233bc443e32b6ea15bd6950
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-09-04 11:16:51 +0100

    Set up server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+
+
+const app = express();
+
+app.use(cors({origin:true, credentials:true}));
+app.use(express.json({extended: false}));
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port ${PORT}`);
+});
